39a41067fc9db9633e5ba9c6bf380eaf2a748475
ilkin <i.abdullaev@nomera.com>
2023-01-04 01:40:20 +0300

    -> Remove unused files

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import linksReducer from '../components/links/linksSlice';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         links: linksReducer
     },
 });
